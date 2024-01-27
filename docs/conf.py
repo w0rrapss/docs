fd7f561d8a6bff41c5dbff89fb5726a55ecc8aa2
w0rraps <54600796+w0rrapss@users.noreply.github.com>
2024-01-27 17:22:30 +0100

    Commented conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -7,39 +7,39 @@ copyright = f"{datetime.date.today().year}, {author}"
 release = "1.0"
 api_version = "1.0"
 
-pygments_style = "sphinx"
-htmlhelp_basename = project
-html_theme_options = {}
-highlight_language = "python3"
+# pygments_style = "sphinx"
+# htmlhelp_basename = project
+# html_theme_options = {}
+# highlight_language = "python3"
 
-extensions = [
-    "sphinx.ext.todo",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.ifconfig",
-    "sphinx.ext.intersphinx"
-]
+# extensions = [
+#     "sphinx.ext.todo",
+#     "sphinx.ext.viewcode",
+#     "sphinx.ext.autodoc",
+#     "sphinx.ext.ifconfig",
+#     "sphinx.ext.intersphinx"
+# ]
 
-rst_prolog = f"""
-.. |api_version| replace:: {api_version}
+# rst_prolog = f"""
+# .. |api_version| replace:: {api_version}
 
-.. role:: pycode(code)
-   :language: python3
-"""
+# .. role:: pycode(code)
+#    :language: python3
+# """
 
 
-exclude_patterns = []
-source_suffix = ".rst"
-master_doc = "index"
+# exclude_patterns = []
+# source_suffix = ".rst"
+# master_doc = "index"
 
-man_pages = [(master_doc, project, f"{project} Documentation", [author], 1)]
-texinfo_documents = [
-    (
-        master_doc,
-        project,
-        f"{project} Documentation",
-        author,
-        project,
-        "w0rraps test docs"
-    ),
-]
+# man_pages = [(master_doc, project, f"{project} Documentation", [author], 1)]
+# texinfo_documents = [
+#     (
+#         master_doc,
+#         project,
+#         f"{project} Documentation",
+#         author,
+#         project,
+#         "w0rraps test docs"
+#     ),
+# ]
